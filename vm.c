7744bf1c48bc58ab304c3f02f40450dcf408d0e0
Mark Magagna <tyrfing666@gmail.com>
2022-02-09 20:35:41 -0500

    compiler with expression support

--- vm.c
+++ vm.c
@@ -33,8 +33,18 @@ Value pop() {
 
 // grab the next opcode and process it.
 InterpretResult interpret(const char* source) {
-    compile(source);
-    return INTERPRET_OK;
+    Chunk chunk;
+    initChunk(&chunk);
+    
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+        return INTERPRET_COMPILE_ERROR;
+    }
+    vm.chunk = &chunk;
+    vm.ip = vm.chunk->code;
+    InterpretResult result = run();
+    freeChunk(&chunk);
+    return result;
 }
 
 // process an opcode.
